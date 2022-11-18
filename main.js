4297c3a25c6d69b95a35c1be386e6c4521ab3e50
Robert <109625312+z00mm00z@users.noreply.github.com>
2022-11-18 20:11:38 +0200

    First attempt at Weather API
    Not working currently - issue with call to API and not with Discord.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const {Client, GatewayIntentBits} = require("discord.js");
+const {Client, GatewayIntentBits, Emoji} = require("discord.js");
 require('dotenv/config')
+
+var weatherOut;
 
 const client = new Client({
     intents: [
@@ -9,6 +11,8 @@ const client = new Client({
     ],
 })
 
+
+
 client.on('ready', () => {
     console.log('Bot is ready.')
 })
@@ -17,6 +21,24 @@ client.on('messageCreate', message => {
     if (message.content == 'ping') {
         message.reply('pong')
     }
+    else if (message.content == 'weather'){
+        FetchWeather('6167865 ')
+        console.log(weatherOut)
+        message.reply('Weather')
+    }
 })
+
+function FetchWeather(_cityName) {
+    var key = '25222afb74d1e9f508fe4c2a71c21d12'
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + _cityName + '&appid=' + key)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+        console.log(data)
+    })
+    .catch(function() {
+        // catch any errors
+    });
+}
+
 
 client.login(process.env.TOKEN)
